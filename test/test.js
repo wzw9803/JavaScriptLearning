8f201059a4cd7c23c5e3f45de359b5ea7698ada4
wzw9803 <wzw9803@163.com>
2020-06-24 22:41:09 +0800

    # 2020.6.24 冲刺开始

--- test/test.js
+++ test/test.js
@@ -1,15 +1,97 @@
-function Foo() {
-  getName = function(){ console.log(1); };
-  return this;
+class Node {
+  constructor(v, next) {
+    this.value = v
+    this.next = next
+  }
 }
-Foo.getName = function() { console.log(2); };
-Foo.prototype.getName = function(){ console.log(3); };
-var getName = function() { console.log(4); };
-function getName(){ console.log(5); }
+class LinkList {
+  constructor() {
+    // 链表长度
+    this.size = 0
+    // 虚拟头部
+    this.dummyNode = new Node(null, null)
+  }
+  find(header, index, currentIndex) {
+    if (index === currentIndex) return header
+    return this.find(header.next, index, currentIndex + 1)
+  }
+  addNode(v, index) {
+    this.checkIndex(index)
+    // 当往链表末尾插入时，prev.next 为空
+    // 其他情况时，因为要插入节点，所以插入的节点
+    // 的 next 应该是 prev.next
+    // 然后设置 prev.next 为插入的节点
+    let prev = this.find(this.dummyNode, index, 0)
+    prev.next = new Node(v, prev.next)
+    this.size++
+    return prev.next
+  }
+  insertNode(v, index) {
+    return this.addNode(v, index)
+  }
+  addToFirst(v) {
+    return this.addNode(v, 0)
+  }
+  addToLast(v) {
+    return this.addNode(v, this.size)
+  }
+  removeNode(index, isLast) {
+    this.checkIndex(index)
+    index = isLast ? index - 1 : index
+    let prev = this.find(this.dummyNode, index, 0)
+    let node = prev.next
+    prev.next = node.next
+    node.next = null
+    this.size--
+    return node
+  }
+  removeFirstNode() {
+    return this.removeNode(0)
+  }
+  removeLastNode() {
+    return this.removeNode(this.size, true)
+  }
+  checkIndex(index) {
+    if (index < 0 || index > this.size) throw Error('Index error')
+  }
+  getNode(index) {
+    this.checkIndex(index)
+    if (this.isEmpty()) return
+    return this.find(this.dummyNode, index, 0).next
+  }
+  isEmpty() {
+    return this.size === 0
+  }
+  getSize() {
+    return this.size
+  }
+}
 
-Foo.getName(); // 2
-getName(); // 4
-Foo().getName(); // 1
-getName(); // 1
-new Foo.getName(); // 2
-new Foo().getName(); // 3
+let list  = new LinkList();
+list.addToLast(10);
+list.addToLast(20);
+list.addToLast(30);
+list.addToLast(40);
+let cur = list.dummyNode;
+let index = 0;
+while(cur !== null){
+  console.log(`${index++}: ${cur.value} - ${cur.next}`);
+  cur = cur.next;
+}
+list.addToFirst(50);
+
+cur = list.dummyNode;
+index = 0;
+while(cur !== null){
+  console.log(`${index++}: ${cur.value} - ${cur.next}`);
+  cur = cur.next;
+}
+
+list.removeLastNode();
+
+cur = list.dummyNode;
+index = 0;
+while(cur !== null){
+  console.log(`${index++}: ${cur.value} - ${cur.next}`);
+  cur = cur.next;
+}
